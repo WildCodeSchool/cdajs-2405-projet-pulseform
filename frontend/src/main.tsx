8c87c29c68e04daa09cc250b4979924e96f7918a
gael <prigentgael@gmail.com>
2024-09-13 12:22:36 +0200

    [Feat]-Configure the frontend environment, Ajouter react avec latest@vite, Apollo client et graphql, configurer codegenn, query, Jwt decode, sass, classnames

---
+++ frontend/src/main.tsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import './index.css';
+
+const client = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
+// const client = ...
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
