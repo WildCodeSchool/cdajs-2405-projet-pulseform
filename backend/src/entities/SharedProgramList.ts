a4044525c04cc91a09a4fa8bcd74881758a769f6
Cyrildab <144703835+Cyrildab@users.noreply.github.com>
2024-11-27 16:34:33 +0100

    added new entities

--- backend/src/entities/SharedProgramList.ts
+++ backend/src/entities/SharedProgramList.ts
@@ -14,20 +14,15 @@ export class SharedProgramList extends BaseEntity {
 
   @Column()
   @Field((type) => Int)
-  friend_id: number;
-
-  @Column()
-  @Field((type) => Int)
   program_id: number;
 
-  @Column()
-  @Field((type) => Int)
-  group_list_id: number;
+  @Column({ nullable: true })
+  @Field((type) => Int, { nullable: true })
+  group_list_id?: number;
 
-  constructor(user_id: number, friend_id: number, program_id: number, group_list_id: number) {
+  constructor(user_id: number, program_id: number, group_list_id?: number) {
     super();
     this.user_id = user_id;
-    this.friend_id = friend_id;
     this.program_id = program_id;
     this.group_list_id = group_list_id;
   }
