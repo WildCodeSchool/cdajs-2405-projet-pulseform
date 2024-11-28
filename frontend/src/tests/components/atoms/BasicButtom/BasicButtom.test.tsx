1f7cad35d386e7a680673f23065d74fead49d4d1
blackstars <nelson.64@live.fr>
2024-11-28 15:45:11 +0100

    update test

--- frontend/src/tests/components/atoms/BasicButtom/BasicButtom.test.tsx
+++ frontend/src/tests/components/atoms/BasicButtom/BasicButtom.test.tsx
@@ -6,9 +6,4 @@ describe("BasicButtom Component", () => {
     const tree = render(<BasicButtom>Button</BasicButtom>);
     expect(tree).toMatchSnapshot();
   });
-
-  it("renders the BasicButtom component with a other width", () => {
-    const tree = render(<BasicButtom width={250}>Button</BasicButtom>);
-    expect(tree).toMatchSnapshot();
-  });
 });
