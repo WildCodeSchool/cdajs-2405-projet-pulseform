69352684d4f131e5859e17e8935fdee7c72cd49c
blackstars <nelson.64@live.fr>
2024-11-28 15:41:00 +0100

    [feat] Landing page - Correction review

--- frontend/src/components/atoms/BasicButton/BasicButton.type.ts
+++ frontend/src/components/atoms/BasicButton/BasicButton.type.ts
@@ -1,7 +1,5 @@
 export interface BasicButtonProps
 	extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	children: React.ReactNode;
-	width?: number;
-	height?: number;
-	fontSize?: number;
+	classnames?: string;
 }
