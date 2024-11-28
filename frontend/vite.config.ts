e43952d885fec0ed32edb154a30abab3b5b7aef0
LuluMena <lucie.menage@gmail.com>
2024-11-28 15:50:23 +0100

    feat-1664-adding CI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,24 +4,27 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		globals: true,
-		// add jsdom to vite
-		environment: 'jsdom',
-		setupFiles: "./src/tests/setup.ts"
-	  },
-	server: {
-		port: 3000,
-		host: "0.0.0.0",
-		proxy: {
-			"/locales": "http://translation:8051",
-		},
-	},
-	resolve: {
-		alias: {
-			"@components": path.resolve(__dirname, "src/components"),
-			"@scss": path.resolve(__dirname, "src/scss"),
-		},
-	},
+  plugins: [react()],
+  test: {
+    globals: true,
+    // add jsdom to vite
+    environment: "jsdom",
+    setupFiles: "./src/tests/setup.ts",
+    coverage: {
+      reporter: ["text", "html"],
+    },
+  },
+  server: {
+    port: 3000,
+    host: "0.0.0.0",
+    proxy: {
+      "/locales": "http://translation:8051",
+    },
+  },
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@scss": path.resolve(__dirname, "src/scss"),
+    },
+  },
 });
