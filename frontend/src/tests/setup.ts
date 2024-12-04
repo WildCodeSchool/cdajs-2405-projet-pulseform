41665bc1bd1092d0c6467466ff6839f676d10ee1
LuluMena <lucie.menage@gmail.com>
2024-12-04 17:14:30 +0100

    pulling App.test.tsx

--- frontend/src/tests/setup.ts
+++ frontend/src/tests/setup.ts
@@ -2,7 +2,7 @@ import "@testing-library/jest-dom/vitest"
 import { afterEach} from 'vitest'
 import { cleanup } from '@testing-library/react'
 
-// runs a clean after each test case (e.g. clearing jsdom)
+// runs a clean after each test case
 afterEach(() => {
   cleanup();
 })
