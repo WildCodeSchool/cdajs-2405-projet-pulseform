4c52ab7b72f5f34dbaa72cd1277a6ffad6651868
LuluMena <lucie.menage@gmail.com>
2025-01-09 17:01:31 +0100

    test precommit

--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -3,10 +3,13 @@ import SecondView from "./components/SecondView";
 import "./LandingPage.scss";
 
 function LandingPage() {
+	const cat = "cat";
+
 	return (
 		<section className="landing-page">
 			<FirstView />
 			<SecondView />
+			<h1>{cat}</h1>
 		</section>
 	);
 }
