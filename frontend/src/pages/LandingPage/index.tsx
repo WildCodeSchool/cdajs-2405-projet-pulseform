ff554df110489880384da317b7e55c2c434c45da
LuluMena <lucie.menage@gmail.com>
2025-01-09 16:52:49 +0100

    test precommit

--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -2,11 +2,7 @@ import FirstView from "./components/FirstView";
 import SecondView from "./components/SecondView";
 import "./LandingPage.scss";
 
-import BasicButton from "@components/atoms/BasicButton";
 function LandingPage() {
-	console.log("test");
-	console.log("test2");
-
 	return (
 		<section className="landing-page">
 			<FirstView />
