429f526a93d4ed59d4a2b3998add227f0bdf4416
LuluMena <lucie.menage@gmail.com>
2025-01-10 09:36:45 +0100

    mise en place biome.json

--- frontend/src/pages/LandingPage/index.tsx
+++ frontend/src/pages/LandingPage/index.tsx
@@ -3,8 +3,6 @@ import SecondView from "./components/SecondView";
 import "./LandingPage.scss";
 
 function LandingPage() {
-	const testUnusedVariable = "testUnusedVariable";
-
 	return (
 		<section className="landing-page">
 			<FirstView />
