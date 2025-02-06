e7ccab0059e32e34ca4031f47b55c2e5495dc272
aglevillayer <levillayer.annegaelle@gmail.com>
2025-02-06 17:17:28 +0100

    init FitnessLevelMenu component

---
+++ frontend/src/components/molecules/FitnessLevelMenu/index.tsx
@@ -0,0 +1,13 @@
+import "./FitnessLevelMenu.scss"
+
+function FitnessLevelMenu(){
+    return (
+        <div className="fitness-level-menu">
+            <button className="fitness-level-menu__button">Débutante</button>
+            <button className="fitness-level-menu__button">Intermédiaire</button>
+            <button className="fitness-level-menu__button">Avancé</button>
+        </div>
+    )
+}
+
+export default FitnessLevelMenu;
