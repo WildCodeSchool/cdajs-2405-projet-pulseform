db0d5cb77ccc7524797cbe361fdea4e21bfeee8e
aglevillayer <levillayer.annegaelle@gmail.com>
2025-03-06 15:20:36 +0100

    wip

--- frontend/src/components/atoms/SelectionButton/SelectionButton.type.ts
+++ frontend/src/components/atoms/SelectionButton/SelectionButton.type.ts
@@ -1,5 +1,5 @@
 export interface SelectionButtonProps
     extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    isActive: boolean;
     children: React.ReactNode;
-    selected?: boolean;
 }
--- frontend/src/components/molecules/FitnessLevelMenu/index.tsx
+++ frontend/src/components/molecules/FitnessLevelMenu/index.tsx
@@ -1,15 +1,25 @@
 import "./FitnessLevelMenu.scss";
 import { SelectionButton } from "../../atoms/SelectionButton/index.";
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 function FitnessLevelMenu() {
   const { t } = useTranslation();
 
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const buttons = [t("BEGINNER"), t("INTERMEDIATE"), t("ADVANCED")];
+
   return (
     <div className="fitness-level-menu">
-      <SelectionButton selected={false}>{t("BEGINNER")}</SelectionButton>
-      <SelectionButton selected={true}>{t("INTERMEDIATE")}</SelectionButton>
-      <SelectionButton selected={false}>{t("ADVANCED")}</SelectionButton>
+      {buttons.map((label, index) => (
+        <SelectionButton
+          key={index}
+          children={label}
+          isActive={activeIndex === index}
+          onClick={() => setActiveIndex(index)}
+        />
+      ))}
     </div>
   );
 }
