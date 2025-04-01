c6046ae2fe4257e3ca85daf354eb5b74a6b3d0ad
aglevillayer <levillayer.annegaelle@gmail.com>
2025-04-01 17:33:01 +0200

    add dotenv for codegen

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 import dotenv from "dotenv";
 
 // Load environment variables from .env file
-//dotenv.config();
+dotenv.config();
 
 const config: CodegenConfig = {
   overwrite: true,
