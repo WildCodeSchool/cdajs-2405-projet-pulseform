7ac0e2e34f6b7678135020cf7acb16eebb6be0dd
aglevillayer <levillayer.annegaelle@gmail.com>
2025-04-01 17:26:41 +0200

    fix errors and refacto

--- frontend/src/pages/HomePage/Views/HomePageView/Views/ProgramListView/index.tsx
+++ frontend/src/pages/HomePage/Views/HomePageView/Views/ProgramListView/index.tsx
@@ -1,17 +1,9 @@
 import ProgramCard from "@components/atoms/ProgramCard";
 import "./ProgramListView.scss";
 import { useGetAllProgramsQuery } from "@graphql/__generated__/schema";
-import dotenv from "dotenv";
-// LIGNE 7 ET 9 A METTRE EN COMMENTAIRE SINON FRONT PLANTE!!
-//dotenv.config();
+
 const ProgramListView = () => {
-  const { loading, error, data } = useGetAllProgramsQuery({
-    context: {
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_GRAPHQL_AUTH_TOKEN}`,
-      },
-    },
-  });
+  const { loading, error, data } = useGetAllProgramsQuery();
 
   if (loading) {
     return <p>Loading...</p>;
