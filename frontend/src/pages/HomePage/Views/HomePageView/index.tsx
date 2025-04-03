42c7dc98016536cd60ed85138b95a943952258b6
aglevillayer <levillayer.annegaelle@gmail.com>
2025-04-03 10:30:06 +0200

    refacto after merging dev

--- frontend/src/pages/HomePage/Views/HomePageView/index.tsx
+++ frontend/src/pages/HomePage/Views/HomePageView/index.tsx
@@ -1,9 +1,12 @@
 import { DashBoardHeaderView, ProgramListView } from "./Views";
+import FitnessLevelMenu from "@components/molecules/FitnessLevelMenu";
+
 import "./HomePageView.scss";
 
 const HomePageView = () => {
   return (
     <div>
+      <FitnessLevelMenu />
       <DashBoardHeaderView />
       <ProgramListView />
     </div>
