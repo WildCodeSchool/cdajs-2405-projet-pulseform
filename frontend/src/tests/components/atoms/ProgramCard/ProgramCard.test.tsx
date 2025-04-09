ab0d259e6c9e4c6038ca913f6b8e37c2f6d51144
LuluMena <lucie.menage@gmail.com>
2025-04-09 10:11:26 +0200

    fix ProgramCard test

--- frontend/src/tests/components/atoms/ProgramCard/ProgramCard.test.tsx
+++ frontend/src/tests/components/atoms/ProgramCard/ProgramCard.test.tsx
@@ -1,6 +1,7 @@
 import ProgramCard from "@components/atoms/ProgramCard";
 import { FitnessLevel, type Program } from "@graphql/__generated__/schema";
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import { describe, expect, it } from "vitest";
 
 describe("ProgramCard Component", () => {
@@ -10,11 +11,15 @@ describe("ProgramCard Component", () => {
     total_duration: 60,
     level: FitnessLevel.Beginner,
     created_at: new Date(),
-    visibility: true,
+    visibility: 0,
   };
 
   it("matches the snapshot", () => {
-    const tree = render(<ProgramCard program={mockProgram} />);
+    const tree = render(
+      <MemoryRouter>
+        <ProgramCard program={mockProgram} />
+      </MemoryRouter>,
+    );
     expect(tree).toMatchSnapshot();
   });
 });
