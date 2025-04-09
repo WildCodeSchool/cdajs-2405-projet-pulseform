716a144f11709b855b0fdbb2fb44a7325c85908d
LuluMena <lucie.menage@gmail.com>
2025-04-09 08:59:14 +0200

    add startprogramview

--- frontend/src/pages/ProgramPage/Views/StartProgramView/index.tsx
+++ frontend/src/pages/ProgramPage/Views/StartProgramView/index.tsx
@@ -1,3 +1,8 @@
+import blobMenGirl from "@assets/images/blob-men-girl.svg";
+import BasicButton from "@components/atoms/BasicButton";
+import ExerciseStepsLayout from "@components/atoms/ExerciseStepsLayout";
+import { useTranslation } from "react-i18next";
+
 import "./StartProgramView.scss";
 
 //EXISTE UNIQUEMENT EN VERSION DESKTOP (C'EST L'IMAGE AU CENTRE DU FRAME 'Exercise List - Desktop' + texte 'Démarre ta séance' + bouton 'Démarrer' SUR FIGMA)
@@ -6,12 +11,31 @@ type StartProgramViewType = {
   onStart: () => void;
 };
 const StartProgramView = ({ onStart }: StartProgramViewType) => {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <p>StartProgramView</p>
-      <button type="button" onClick={onStart}>
-        start
-      </button>
+      <ExerciseStepsLayout
+        header={
+          <div className="start-program-view__header">
+            <span>Démarre ta séance</span>
+          </div>
+        }
+        body={
+          <div className="start-program-view__body">
+            <img
+              src={blobMenGirl}
+              alt="Start Program"
+              className="start-program-view-image"
+            />
+          </div>
+        }
+        footer={
+          <div className="start-program-view__footer">
+            <BasicButton onClick={onStart}>{t("START_PROGRAM")}</BasicButton>
+          </div>
+        }
+      />
     </div>
   );
 };
