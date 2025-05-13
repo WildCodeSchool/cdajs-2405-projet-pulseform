d85fecac8f0254e4a859ff777ce72d104de5bb7f
LuluMena <lucie.menage@gmail.com>
2025-05-14 00:52:07 +0200

    remove dead code

--- frontend/src/components/atoms/ProgramCard/index.tsx
+++ frontend/src/components/atoms/ProgramCard/index.tsx
@@ -1,4 +1,3 @@
-import { forwardRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 import DurationLabel from "../DurationLabel";
@@ -13,38 +12,33 @@ type ProgramCardProps = {
   program: Program;
 };
 
-const ProgramCard = forwardRef<HTMLButtonElement, ProgramCardProps>(
-  ({ program }, ref) => {
-    const convertedDuration = convertSecondsToMinRounded(
-      program.total_duration,
-    );
-    const navigate = useNavigate();
+const ProgramCard = ({ program }: ProgramCardProps) => {
+  const convertedDuration = convertSecondsToMinRounded(program.total_duration);
+  const navigate = useNavigate();
 
-    return (
-      <button
-        type="button"
-        className="program-card"
-        onClick={() => navigate(`/program/${program.id}`)}
-        ref={ref}
-      >
-        <img
-          src={program?.image || ""}
-          alt={program?.name || ""}
-          className="program-card__image"
-        />
-        <div className="program-card__overlay">
-          <div className="program-card__header">
-            <DurationLabel duration={convertedDuration || ""} />
-            <FlamesFitnessLevel level={program?.level} />
-          </div>
-          <div className="program-card__title_container">
-            <h3 className="program-card__title">{program?.name}</h3>
-          </div>
+  return (
+    <button
+      type="button"
+      className="program-card"
+      onClick={() => navigate(`/program/${program.id}`)}
+    >
+      <img
+        src={program?.image || ""}
+        alt={program?.name || ""}
+        className="program-card__image"
+      />
+      <div className="program-card__overlay">
+        <div className="program-card__header">
+          <DurationLabel duration={convertedDuration || ""} />
+          <FlamesFitnessLevel level={program?.level} />
         </div>
-      </button>
-    );
-  },
-);
+        <div className="program-card__title_container">
+          <h3 className="program-card__title">{program?.name}</h3>
+        </div>
+      </div>
+    </button>
+  );
+};
 
 ProgramCard.displayName = "ProgramCard";
 
