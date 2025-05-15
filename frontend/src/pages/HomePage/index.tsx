6fccfbe37e9a49e170ec192132c3476e9e339105
aglevillayer <levillayer.annegaelle@gmail.com>
2025-05-15 15:32:48 +0200

    wip on blob + icon menu

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,9 +1,11 @@
+import blobHomePage from "@assets/blobs/blob_homePage.svg";
 import DoubleScreenLayout from "@components/atoms/Layout/DoubleScreenLayout";
 import MobileBodyLayout from "@components/atoms/Layout/MobileBodyLayout";
 import { useHomeMobileView } from "@context/MobileHomeViewContext";
 import { useEffect, useState } from "react";
 import { HomePageView, UserProfileView } from "./Views";
 import "./HomePage.scss";
+import { MenuBurgerIcon } from "@utils/icon-list/iconList";
 
 const HomePage = () => {
   const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
@@ -21,11 +23,29 @@ const HomePage = () => {
     };
   }, []);
 
+  const onMenu = () => {
+    console.log("on menu");
+  };
+
   return (
     <>
       <section className={isDesktop ? "desktop" : "mobile"}>
         {isDesktop ? (
           <>
+            <img
+              className="desktop__blob"
+              src={blobHomePage}
+              alt="blob"
+              aria-hidden="true"
+            />
+            <MenuBurgerIcon
+              className="desktop__menuBurgerIcon"
+              color="white"
+              fontSize={40}
+              onClick={() => {
+                onMenu();
+              }}
+            />
             <DoubleScreenLayout>
               <UserProfileView isDesktop={isDesktop} />
               {/* Left column with user profile */}
